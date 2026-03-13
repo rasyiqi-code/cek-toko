e2098263c6f1c0e06a96bcb2669ebb13f9f95cd1
rasyiqi <retaslintasbatas@gmail.com>
2026-03-14 01:33:11 +0700

    build: production APK generated with web shell and premium branding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,7 @@ const config: CapacitorConfig = {
   server: {
     // url: 'http://10.19.13.235:3000', // Gunakan IP lokal untuk testing offline di WiFi
     androidScheme: 'https',
+    url: 'https://cek-toko.vercel.app',
     cleartext: true
   }
 };
